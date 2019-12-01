67807aa98077f16218aa25d00b1a58faf3bb4100
i-danunzio <idanunzio@gmail.com>
2019-12-01 20:50:47 -0300

    sumamos las rutas y los controllers

--- myApp/routes/main.js
+++ myApp/routes/main.js
@@ -4,8 +4,26 @@ const router = express.Router();
 
 // ************ Controller Require ************
 const mainController = require('../controllers/mainController');
+const productAddController = require('../controllers/productAddController');
+const productCartController = require('../controllers/productCartController');
+const productDetailController = require('../controllers/productDetailController');
+const registerController = require('../controllers/registerController');
+
 
 /* GET - home page. */
 router.get('/', mainController.root);
+
+/* GET - home page. */
+router.get('/productDetail',productDetailController.root);
+
+/* GET - home page. */
+router.get('/productCart',productCartController.root);
+
+/* GET - home page. */
+router.get('/register',registerController.root);
+
+/* GET - home page. */
+router.get('/productAdd',productAddController.root);
+
 
 module.exports = router;
