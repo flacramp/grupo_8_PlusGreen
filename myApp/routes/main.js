5735b28794c760ed542ac392526e72623bb36c59
i-danunzio <idanunzio@gmail.com>
2019-12-05 23:30:31 -0300

    Subo las rutas del header y footer -ID

--- myApp/routes/main.js
+++ myApp/routes/main.js
@@ -8,6 +8,8 @@ const productAddController = require('../controllers/productAddController');
 const productCartController = require('../controllers/productCartController');
 const productDetailController = require('../controllers/productDetailController');
 const registerController = require('../controllers/registerController');
+const footerController = require('../controllers/footerController');
+const headerController = require('../controllers/headerController');
 
 
 /* GET - home page. */
@@ -24,6 +26,12 @@ router.get('/register',registerController.root);
 
 /* GET - home page. */
 router.get('/productAdd',productAddController.root);
+
+/* GET - footer. */
+router.get('/footer',footerController.root);
+
+/* GET - footer. */
+router.get('/header',headerController.root);
 
 
 module.exports = router;
